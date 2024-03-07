b93d74f28dfaa57e4aaecfbdd9d63cd13e8b8b62
Gabriel <63365858+GabrielDemesio@users.noreply.github.com>
2024-03-07 09:11:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,19 +5,19 @@ document.getElementById("resetButton").addEventListener("click", function(){
 });
 
     const brand = document.querySelector('input[name="brand"]:checked').value;
-    const value = document.querySelector('input[name="value"]:checked').value;
+    const price = document.querySelector('input[name="price"]:checked').value;
     const size = document.querySelector('input[name="size"]:checked').value;
 
     // let recommendation = "";
 
 // Mapeamento das combinações de entrada para as recomendações
 const recommendations = {
-    "AMD_1000_pequena": "Ryzen 3 4100",
-    "Intel_1000_pequena": "i3 12100f",
-    "Intel_2000_media": "Intel® Core™ i5-12600K",
-    "AMD_2000_media": "Ryzen 5 5600",
-    "AMD_3000_grande": "Ryzen 7 7700x",
-    "Intel_3000_grande": "Intel Core I7 13700k"
+    "AMD_1000_small": "Ryzen 3 4100",
+    "Intel_1000_small": "i3 12100f",
+    "Intel_2000_medium": "Intel® Core™ i5-12600K",
+    "AMD_2000_medium": "Ryzen 5 5600",
+    "AMD_3000_big": "Ryzen 7 7700x",
+    "Intel_3000_big": "Intel Core I7 13700k"
   };
   
   // Verifica se a combinação existe no mapeamento, caso contrário, retorna a mensagem padrão
